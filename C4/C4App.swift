7f90e5a3105c8ab4c45da65e5e52285aed6c6e9c
Nathan Chuluda <nchuluda@gmail.com>
2024-09-30 13:07:25 -0400

    Initial Commit

---
+++ C4/C4App.swift
@@ -0,0 +1,17 @@
+//
+//  C4App.swift
+//  C4
+//
+//  Created by Nathan on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct C4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
